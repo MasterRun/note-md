98a3ea71b35c04fa5bb3bc0bb8779fe398793549
jsongo <jsongo97@outlook.com>
2021-07-02 19:36:02 +0800

    更新笔记

--- mac/android_run.sh
+++ mac/android_run.sh
@@ -1,18 +1,32 @@
-#/bin/zsh
+#!/bin/zsh
 echo "start run android "
 #echo "\$0 is $0"
 #echo "\$1 is $1"
 echo $*
 adb devices
 
+containsElement () {
+
+  local e match="$1"
+
+  shift
+
+  for e; do [[ "$e" == "$match" ]] && return 0; done
+
+  return 1
+
+}
+
+containsElement 
+
 # clean
-./gradlew clean
+#./gradlew clean
 
 # assemblePackage
-./gradlew assembleSitDebug
+#./gradlew assembleSitDebug
 
 # install
-adb install -r -t app/build/outputs/apk/sit/debug/app-sit-debug.apk
+#adb install -r -t app/build/outputs/apk/sit/debug/app-sit-debug.apk
 
 # launch
-adb shell am start com.dejiplaza.deji/.ui.welcome.FlashActivity
+#adb shell am start com.dejiplaza.deji/.ui.welcome.FlashActivity
