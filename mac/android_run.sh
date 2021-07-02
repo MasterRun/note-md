8c33188f7eb96f0a4938aaac7c3859b7d778daff
jsongo <jsongo97@outlook.com>
2021-07-02 18:46:22 +0800

    更新mac相关笔记

---
+++ mac/android_run.sh
@@ -0,0 +1,18 @@
+#/bin/zsh
+echo "start run android "
+#echo "\$0 is $0"
+#echo "\$1 is $1"
+echo $*
+adb devices
+
+# clean
+./gradlew clean
+
+# assemblePackage
+./gradlew assembleSitDebug
+
+# install
+adb install -r -t app/build/outputs/apk/sit/debug/app-sit-debug.apk
+
+# launch
+adb shell am start com.dejiplaza.deji/.ui.welcome.FlashActivity
